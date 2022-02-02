c3d5c0e529036c119007017f899fa664a4dd9050
NitaLewska <Nita@ya.ru>
2022-02-02 21:34:37 +0300

    fix: src

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Patient_card from './Patient_card'
 import Timer from './Timer'
+import photo from './img/owl_depressed.jpg'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Patient_card name="Бла" surname="Блабла" birth="неизвестен" gender="неизвестен" illness="депресука" photo="./src/img/owl_depressed.jpg"/>
+    <Patient_card name="Бла" surname="Блабла" birth="неизвестен" gender="неизвестен" illness="депресука" photo={photo}/>
   </React.StrictMode>,
   document.getElementById('root')
 )
