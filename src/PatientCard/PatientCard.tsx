1bf1a3c765a56e1b8055ef673be96027182b3350
NitaLewska <Nita@ya.ru>
2022-02-05 03:41:40 +0300

    fix/refactor: add props interface+destructuring

--- src/PatientCard/PatientCard.tsx
+++ src/PatientCard/PatientCard.tsx
@@ -1,16 +1,26 @@
 import { ReactChild, ReactFragment, ReactPortal } from 'react'
 import './PatientCard.css'
 
-function PatientCard(props: { photo: string | undefined; name: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; surname: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; birth: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; gender: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; illness: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined }) {
+interface MyProps {
+  name: string;
+  surname: string;
+  birth: number;
+  gender: string;
+  illness: string;
+  photo: string;
+}
+
+
+function PatientCard({name, surname, birth, gender, illness, photo}:MyProps) {
     return (
     <section className="patient_card">
-      <img src={props.photo} alt="" className="patient_card__photo"/>
+      <img src={photo} alt="" className="patient_card__photo"/>
       <article className="patient_card__article">
-        <p><b>Имя: </b>{props.name}</p>
-        <p><b>Фамилия: </b>{props.surname}</p>
-        <p><b>Год рождения: </b>{props.birth}</p>
-        <p><b>Пол: </b>{props.gender}</p>
-        <p><b>Диагноз: </b>{props.illness}</p>
+        <p><b>Имя: </b>{name}</p>
+        <p><b>Фамилия: </b>{surname}</p>
+        <p><b>Год рождения: </b>{birth}</p>
+        <p><b>Пол: </b>{gender}</p>
+        <p><b>Диагноз: </b>{illness}</p>
       </article>
     </section>
   )
