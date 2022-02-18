ab8a671af82589874312054d9513c511b042a2a2
NitaLewska <Nita@ya.ru>
2022-02-18 15:49:15 +0300

    prettier

--- src/PatientCard/PatientCard.tsx
+++ src/PatientCard/PatientCard.tsx
@@ -1,31 +1,35 @@
-import './PatientCard.css';
-import PatientCardRow from "../PatientCardRow/PatientCardRow"
+import "./PatientCard.css";
+import PatientCardRow from "../PatientCardRow/PatientCardRow";
 
 interface MyProps {
   name: string;
   surname: string;
-  birth: number|string;
+  birth: number | string;
   gender: string;
   illness: string;
   photo: string;
 }
 
-
-
-
-function PatientCard({name, surname, birth, gender, illness, photo}:MyProps) {
-    return (
+function PatientCard({
+  name,
+  surname,
+  birth,
+  gender,
+  illness,
+  photo,
+}: MyProps) {
+  return (
     <section className="patient_card">
-      <img src={photo} alt="" className="patient_card__photo"/>
+      <img src={photo} alt="" className="patient_card__photo" />
       <article className="patient_card__article">
-        <PatientCardRow text="Имя: " propName={name}  />
-        <PatientCardRow text="Фамилия: " propName={surname}  />
-        <PatientCardRow text="Год рождения: " propName={birth}  />
-        <PatientCardRow text="Пол: " propName={gender}  />
-        <PatientCardRow text="Диагноз: " propName={illness}  />
+        <PatientCardRow text="Имя: " propName={name} />
+        <PatientCardRow text="Фамилия: " propName={surname} />
+        <PatientCardRow text="Год рождения: " propName={birth} />
+        <PatientCardRow text="Пол: " propName={gender} />
+        <PatientCardRow text="Диагноз: " propName={illness} />
       </article>
     </section>
-  )
+  );
 }
 
-export default PatientCard
+export default PatientCard;
--- src/Timer/Timer.tsx
+++ src/Timer/Timer.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from "react-dom";
-import './Timer.css'
+import "./Timer.css";
 
 function Timer() {
-const text_n_clock = (
-    <p className="text_n_clock">В палате сейчас {new Date().toLocaleTimeString()}</p>
-)
-  ReactDOM.render( text_n_clock, document.getElementById('time')
-  )
+  const text_n_clock = (
+    <p className="text_n_clock">
+      В палате сейчас {new Date().toLocaleTimeString()}
+    </p>
+  );
+  ReactDOM.render(text_n_clock, document.getElementById("time"));
 }
 
-export default Timer
+export default Timer;
